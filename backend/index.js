a71f6d8347a776a5ef44ca9a939abd0c855a597d
MUNEEB-AYYAN <muneebayyan348@gmail.com>
2025-07-21 22:16:11 +0530

    vercel and cors correction

--- backend/index.js
+++ backend/index.js
@@ -22,13 +22,10 @@ connectDB();
 const app = express();
 
 // CORS Middleware
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://matrimon-xpmw.vercel.app"],
-    credentials: true,
-  })
-);
-
+app.use(cors({
+  origin: ['https://matrimon-xpmw.vercel.app'],
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
   res.send('Welcome to Matrimony Backend API 1_');
@@ -52,12 +49,11 @@ app.use(errorHandler);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://matrimon-xpmw.vercel.app"],
+    origin: "https://matrimon-xpmw.vercel.app",
     methods: ["GET", "POST"],
-    credentials: true,
-  },
+    credentials: true
+  }
 });
-
 
 // Socket events
 io.on('connection', (socket) => {
